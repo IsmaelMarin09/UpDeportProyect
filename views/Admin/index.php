979f8230e16c110b16eba0cf557f99da79e33037
juampa996 <75534230+juampa996@users.noreply.github.com>
2021-10-23 11:51:02 -0500

    sesion nombres

--- views/Admin/index.php
+++ views/Admin/index.php
@@ -1,4 +1,28 @@
+<?php
 
+require_once('class.conexion.php');
+
+  session_start();
+  $rows=null;
+  $modelo= new Conexion();
+  $conexion=  $modelo->get_conexion();
+  $id="%".$_SESSION['id']."%";
+  $sql="select * FROM users where id like :id";
+  $statement= $conexion-> prepare($sql);
+  $statement->bindParam(":id", $id);
+  $statement -> execute();
+  $result= $statement->fetch();
+  $rows[]=$result;
+  
+  foreach ($rows as $row){
+    $nombre=$row['nombre'];
+  }
+ 
+  
+
+  
+
+?>
 <!DOCTYPE html>
 <html lang="es">
   <head>
@@ -83,7 +107,7 @@
             <a
               class="nav-link"
              
-              href="/views/administration-side/DashBoard//search/enhanced.php"
+              href="/search/enhanced.php"
               role="button"
             >
               <i class="fas fa-search"></i>
