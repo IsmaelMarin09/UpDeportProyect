014e49e190f3bcaca12117f635edd7b6e878f522
Pablo Cifuentes <juanpacifuentes1999@gmail.com>
2021-11-06 11:09:54 -0500

    Admin
    parte de admin

--- Controller/listarUsersAdmin.php
+++ Controller/listarUsersAdmin.php
@@ -5,53 +5,62 @@
       $resut=$objetoConsultas->listarUsers();
 
       if (!isset($result)) {
+        echo '
+        <thead>
+        <tr>
+          <th>ID</th>
+          <th>unique_ID</th>
+          <th>nombre</th>
+          <th>Correo</th>
+          <th>Rol</th>
+          <th>deporte</th>
+          <th>Fecha</th>
+          <th>lugar</th>
+          <th>Genero</th>
+          <th>Estado</th>
+          <th>Deshabilitar</th>
+          <th>Modificar</th>
+        </tr>
+        </thead>
 
-          echo '<h2>NO HAY DATOD</h2>';
-      }else{
-          echo '
-          <thead>
-          <tr>
-            <th>ID</th>
-            <th>ID</th>
-            <th></th>
-            <th>Engine version</th>
-            <th>CSS grade</th>
-          </tr>
-          </thead>
+        <tbody>
+                
+       
+                
+        ';
 
-          <tbody>
-                  
+        foreach ($resut as $f) {
+            echo "<tr>";
+            echo "<td>" .$f['id']."</td>";
+            echo "<td>" .$f['unique_Id']."</td>";
+            echo "<td>" .$f['nombre']."</td>";
+            echo "<td>" .$f['email']."</td>";
+            echo "<td>" .$f['rol']."</td>";
+            echo "<td>" .$f['deporte']."</td>";
+            echo "<td>" .$f['fNacimiento']."</td>";
+            echo "<td>" .$f['municipio']."</td>";
+            echo "<td>" .$f['genero']."</td>";
+            echo "<td>" .$f['estado']."</td>";
+            echo " <td><a href='../Controlador/eliminar.php?id_producto=".$f['id']."'>Deshabilitar </td>";
+            echo " <td><a href='../Controlador/modificar.php?id_producto=".$f['id']."'>Modificar </td>";
+            echo "</tr>";
+
+
+    };
+    echo '</tbody>';
+
+
+
+
+
+   }
+   else{
+    echo '<h2>NO HAY DATOD</h2>';
+ } ;
+
+
          
-                  
-          ';
-
-          foreach ($resut as $f) {
-              echo "<tr>";
-              echo "<td>" .$f['id']."</td>";
-              echo "<td>" .$f['unique_id']."</td>";
-              echo "<td>" .$f['nombre']."</td>";
-              echo "<td>" .$f['email']."</td>";
-              echo "<td>" .$f['clave']."</td>";
-              echo "<td>" .$f['img']."</td>";
-              echo "<td>" .$f['rol']."</td>";
-              echo "<td>" .$f['deporte']."</td>";
-              echo "<td>" .$f['fNacimiento']."</td>";
-              echo "<td>" .$f['municipio']."</td>";
-              echo "<td>" .$f['genero']."</td>";
-              echo "<td>" .$f['estado']."</td>";
-              echo "</tr>";
-
-
-      };
-      echo '</tbody>';
-
-
-
-
-  
-     };
-     } ;
-
+      }
 
   
 
--- Model/consutasAdmin.php
+++ Model/consutasAdmin.php
@@ -1,19 +1,18 @@
 <?php
 require_once("conexion.php");
  class consultasAdmin{
-     public function listarUsers(){
+      public function listarUsers(){
+       
          $f=null;
-
          $objetoConexion=new conexion();
          $conexion=$objetoConexion->get_conexion();
-
-
          $listar="SELECT * FROM  users";
          $statement=$conexion->prepare ($listar);
          $statement ->execute();
 
          while ($resultado=$statement->fetch()) {
              $f[]=$resultado;
+            
          }
          return $f;
 
