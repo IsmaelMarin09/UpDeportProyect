dee20e9889eacf572b854a82167652847568b9e4
Pablo Cifuentes <juanpacifuentes1999@gmail.com>
2021-11-21 12:38:19 -0500

    arreglo de errores pqrs

--- Controller/deneSoliHunter.php
+++ Controller/deneSoliHunter.php
@@ -2,11 +2,12 @@
  require_once('../Model/conexion.php');
  require_once('../Model/consutasAdmin.php');
 $id=$_GET['id_hunter']; 
-echo $id;
+
 if (isset($id)) {
     $objetoConsultas= new consultasAdmin();
     $result =$objetoConsultas-> ModInfoPerfil("estado","Denegada",$id,"documentoshunters");
     $result2 =$objetoConsultas-> ModInfoPerfil("estado","Inactivo",$id,"users");
+    echo "<script>location.href='../views/Admin/autorizarCuentas.php' </script>"  ;
 }else {
     echo  "<script> alert('Por favor no intente ingresar por este link puede producir fallos') </script>";
     echo "<script>location.href='../views/Admin/listarSoliHunters.php"  ;
--- Controller/listarSoliHunters.php
+++ Controller/listarSoliHunters.php
@@ -54,11 +54,58 @@ echo '</tbody>';
 
    
  } ;
+ function cargarUsers2(){
+ $objetoConsultas= new consultasAdmin();
+ $result=$objetoConsultas->listarTablas("documentoshunters");
+ if (!isset($result)) {
+   echo '<h2>NO HAY DATOS</h2>';
 
+}
+else{
+echo '
+<thead>
+<tr>
+ <th>ID</th>
+ <th>unique_ID</th>
+ <th>Cedula</th>
+ <th>Hoja de vida</th>
+ <th>Certificación laboral</th>
+ <th>Estado</th>
+
+ 
+
+</tr>
+</thead>
+
+<tbody>
+       
+
+       
+';
+
+foreach ($result as $f) {
+   echo "<tr>";
+   echo "<td>" .$f['id']."</td>";
+   echo "<td>" .$f['unique_Id']."</td>";
+   echo "<td><a href='../Assets/img/documentos_hunters/".$f['unique_Id']."cc.pdf'' target='_blank'>" .$f['cedula']."</td>";
+   echo "<td><a href='../Assets/img/documentos_hunters/".$f['unique_Id']."hv.pdf'' target='_blank'>" .$f['hojaDeVida']."</td>";
+   echo "<td><a href='../Assets/img/documentos_hunters/".$f['unique_Id']."cl.pdf'' target='_blank'>" .$f['certificacionLaboral']."</td>";
+   echo "<td>" .$f['estado']."</td>";
+   
+  
+  
+   echo "</tr>";
+
+
+};
+echo '</tbody>';
+
+
+} ;
 
          
       }
 
-  
+}
 
 ?>
